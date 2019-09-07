309f373b224cf0a414842643ba0a36efad3cb8ef
Thealeshka <alexfenin1234@gmail.com>
2019-09-08 01:10:58 +0300

    add

--- src/test/java/com/aleshka/demo/IpTestApplicationTests.java
+++ src/test/java/com/aleshka/demo/IpTestApplicationTests.java
@@ -1,16 +1,28 @@
 package com.aleshka.demo;
 
+import com.aleshka.demo.entity.User;
+import com.aleshka.demo.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IpTestApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
 
+    @Test
+    public void test() {
+        User user=new User("test", 1234);
+        userRepository.save(user);
+        Assert.isTrue(userRepository.findAll().contains(user),"if contains its ok");
 
+    }
 
 }
